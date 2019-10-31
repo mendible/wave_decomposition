b53373779f78aadc6528a7a9113693f55191e02a
mendible <mendible@uw.edu>
2019-10-30 17:05:46 -0700

    finalizing before arxiv

--- initialize_C.m
+++ initialize_C.m
@@ -1,23 +1,23 @@
-function [Ain] = initA(x,t,W,k,n,lambda,library)
+function [Cin] = initialize_C(x,t,W,k,n,lambda,library)
 % initA initializes the SR3 optimization using an iteratively thresholded
 % least squares
 
-Ain = zeros(k,n);
+Cin = zeros(k,n);
 
 for jj = 1:n
     ttmp = t(logical(W(:,jj)));
     xtmp = x(logical(W(:,jj)));
     Ttmp = library(ttmp);
-    Aincol = Ttmp\xtmp; % initial guess: Least-squares
+    Cincol = Ttmp\xtmp; % initial guess: Least-squares
     % lambda is our sparsification knob.
     for iter = 1:10
-        smallinds = (abs(Aincol)<lambda); % find small coefficients
-        Aincol(smallinds)=0; % and threshold
+        smallinds = (abs(Cincol)<lambda); % find small coefficients
+        Cincol(smallinds)=0; % and threshold
         biginds = ~smallinds(:);
         % Regress dynamics onto remaining terms to find sparse A
-        Aincol(biginds) = Ttmp(:,biginds)\xtmp(:);
+        Cincol(biginds) = Ttmp(:,biginds)\xtmp(:);
     end
-    Ain(:,jj) = Aincol;
+    Cin(:,jj) = Cincol;
 end
 
 end
